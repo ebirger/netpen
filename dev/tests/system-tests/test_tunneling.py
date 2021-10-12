ef765534fe98ef845db8eb3321eba34a961ecb25
Eyal Birger <eyal.birger@gmail.com>
2021-10-12 06:49:43 +0300

    tests: make sure l2tp modules are loadable before running l2tp test
    Not supported in github actions.

--- dev/tests/system-tests/test_tunneling.py
+++ dev/tests/system-tests/test_tunneling.py
@@ -2,27 +2,35 @@
 # pylint: disable=unused-argument
 import pytest
 from conftest import get_ns_addr_in_subnet, get_subnet, deploy_script
-from conftest import ping, get_route_dev, get_xfrm_packet_counts
+from conftest import ping, get_route_dev, get_xfrm_packet_counts, modprobe
 from dev.tests.common.utils import gen_examples  # pylint: disable=unused-import
 
 
 OVERLAY_SUBNET_NAME = 'overlay'
-TUNNEL_PARAMS = [('ipip', 'overlay', 'zone1', 'zone2', 'tnl0', True, False),
-                 ('ipip6', 'overlay', 'zone1', 'zone2', 'tnl0', True, False),
-                 ('gre', 'overlay', 'zone1', 'zone2', 'tnl0', True, False),
-                 ('vti', 'green', 'alice', 'bob', 'safe', True, True),
-                 ('vti6', 'green', 'alice', 'bob', 'safe', True, True),
-                 ('xfrmi', 'green', 'alice', 'bob', 'safe', True, True),
-                 ('gre_xfrm', 'overlay', 'zone1', 'zone2', 'tnl0', True, True),
-                 ('wireguard', 'green', 'alice', 'bob', 'safe', True, False),
-                 ('vxlan', 'overlay', 'zone1', 'zone2', 'tnl0', False, False),
-                 ('l2tp', 'overlay', 'zone1', 'zone2', 'tnl0', False, False)]
+TUNNEL_PARAMS = [('ipip', 'overlay', 'zone1', 'zone2', 'tnl0', True, False, []),
+                 ('ipip6', 'overlay', 'zone1', 'zone2', 'tnl0', True, False,
+                     []),
+                 ('gre', 'overlay', 'zone1', 'zone2', 'tnl0', True, False, []),
+                 ('vti', 'green', 'alice', 'bob', 'safe', True, True, []),
+                 ('vti6', 'green', 'alice', 'bob', 'safe', True, True, []),
+                 ('xfrmi', 'green', 'alice', 'bob', 'safe', True, True, []),
+                 ('gre_xfrm', 'overlay', 'zone1', 'zone2', 'tnl0', True, True,
+                     []),
+                 ('wireguard', 'green', 'alice', 'bob', 'safe', True, False,
+                     []),
+                 ('vxlan', 'overlay', 'zone1', 'zone2', 'tnl0', False, False,
+                     []),
+                 ('l2tp', 'overlay', 'zone1', 'zone2', 'tnl0', False, False,
+                     ['l2tp_core', 'l2tp_ip', 'l2tp_netlink', 'l2tp_eth'])]
 
 
-@pytest.mark.parametrize('name,overlay,z1,z2,dev,is_l3,check_xfrm',
+@pytest.mark.parametrize('name,overlay,z1,z2,dev,is_l3,check_xfrm,modules',
                          TUNNEL_PARAMS)
-def test_tunnel(name, overlay, z1, z2, dev, is_l3, check_xfrm, gen_examples,
-                cleanup_nets):
+def test_tunnel(name, overlay, z1, z2, dev, is_l3, check_xfrm, modules,
+                gen_examples, cleanup_nets):
+    if not all(modprobe(m) for m in modules):
+        pytest.skip('unsupported modules')
+
     deploy_script(f'{name}.sh')
 
     subnet = get_subnet(name, overlay)
