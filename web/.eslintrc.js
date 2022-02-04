29afc597404279539dd1df05e081c0e0c5416a03
Eyal Birger <eyal.birger@gmail.com>
2022-02-04 11:16:07 +0200

    web: upgrade react-scripts to 5.0.0
    Needed changes:
    
    - added "assert", "buffer", and "stream" because new webpack doesn't provide
      them as polyfills
    - upgraded react-responsive and core-util-is for supporting new webpack
    - use "useLocation" from "react-router-dom" instead of "react"

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   "globals": {
     "process": true,
-    "require": true,
     "module": true
   },
   "env": {
--- web/src/components/Share.js
+++ web/src/components/Share.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLocation } from 'react';
+import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Button, message } from 'antd';
 import { LinkOutlined } from '@ant-design/icons';
