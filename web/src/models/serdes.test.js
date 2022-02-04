63c47eae8f52dbeeb9065b60ffd3bf8becf4cbd7
Eyal Birger <eyal.birger@gmail.com>
2022-02-04 13:51:59 +0200

    web: add simple serialization tests
    Check that examples can be loaded and serialized correctly by
    the models layer.

---
+++ web/src/models/serdes.test.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import YAML from 'yaml';
+import { loadObjList } from '../models/Loader.js';
+import { serializeObjList } from '../models/ObjModel.js';
+
+function getPaths() {
+  const fname = `../examples/example_list.yml`;
+  const content = fs.readFileSync(fname, "utf8", function(err, data) {
+    return data;
+  });
+  
+  const o = YAML.parse(content);
+  let paths = [];
+  o.forEach((x) => {
+    x.items.forEach((i) => {
+      paths = paths.concat(i.path);
+    });
+  });
+  return paths;
+}
+
+
+describe('Serialization tests', () => {
+  const paths = getPaths();
+  it.each(paths.map((x) => [x]))("serdes %s", (example) => {
+    const fname = `../examples/${example}`;
+    const content = fs.readFileSync(fname, "utf8", function(err, data) {
+      return data;
+    });
+
+    const y = YAML.parse(content);
+    const loaded = loadObjList(y);
+    const serialized = serializeObjList(loaded);
+    expect(serialized).toEqual(y.items);
+  });
+});
