9edf9c567227e45d232249eeb4b8742a9186fe61
Eyal Birger <eyal.birger@gmail.com>
2022-02-18 17:09:24 +0200

    system-tests: test tunnel devices in different ns
    Test moving the tunnel device to a different NS.

--- dev/tests/system-tests/test_tunneling.py
+++ dev/tests/system-tests/test_tunneling.py
@@ -3,7 +3,9 @@
 import pytest
 from conftest import get_ns_addr_in_subnet, get_subnet, deploy_script
 from conftest import ping, get_route_dev, get_xfrm_packet_counts, modprobe
+from conftest import load_example
 from dev.tests.common.utils import gen_examples  # pylint: disable=unused-import
+from dev.tests.common.utils import netpen_yaml_to_bash
 
 
 OVERLAY_SUBNET_NAME = 'overlay'
@@ -49,3 +51,44 @@ def test_tunnel(name, overlay, z1, z2, dev, is_l3, check_xfrm, modules,
 
     assert get_route_dev(z1, z2incidr) == f'{dev}.dev1'
     assert get_route_dev(z2, z1incidr) == f'{dev}.dev2'
+
+
+@pytest.mark.parametrize('name,overlay,z1,z2,dev,is_l3,check_xfrm,modules',
+                         TUNNEL_PARAMS)
+def test_tunnel_different_ns(name, overlay, z1, z2, dev, is_l3, check_xfrm,
+                             modules, gen_examples, cleanup_nets):
+
+    different_ns = 'different_ns'
+
+    if not all(modprobe(m) for m in modules):
+        pytest.skip('unsupported modules')
+
+    y = load_example(name)
+
+    # move one of the tunnel devices to a different ns
+    t = next(i['tunnel'] for i in y['items'] if 'tunnel' in i)
+    t['dev1'] = dict(netns=f'netns.{different_ns}')
+    y['items'].append(dict(netns=dict(name=different_ns)))
+
+    script = f'{name}_{different_ns}.sh'
+    netpen_yaml_to_bash(y, script)
+
+    deploy_script(script)
+
+    subnet = get_subnet(name, overlay)
+
+    diffnsincidr, diffnsnet = get_ns_addr_in_subnet(different_ns, subnet)
+    z2incidr, z2net = get_ns_addr_in_subnet(z2, subnet)
+
+    assert is_l3 == (diffnsnet != z2net)
+
+    ping(different_ns, z2incidr)
+    ping(z2, diffnsincidr)
+
+    if check_xfrm:
+        # state is still in z1, so we get packet count from there
+        assert get_xfrm_packet_counts(z1) == [2, 2]
+        assert get_xfrm_packet_counts(z2) == [2, 2]
+
+    assert get_route_dev(different_ns, z2incidr) == f'{dev}.dev1'
+    assert get_route_dev(z2, diffnsincidr) == f'{dev}.dev2'
